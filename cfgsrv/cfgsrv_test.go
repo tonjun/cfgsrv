d249be1da77edb97a1a738301c8b6da2a697ecac
Tonjun Banaag <tonjun@gmail.com>
2017-05-22 17:26:32 +0800

    Add some more tests

--- cfgsrv/cfgsrv_test.go
+++ cfgsrv/cfgsrv_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/onsi/gomega/gbytes"
 	"github.com/tonjun/cfgsrv"
+	"github.com/tonjun/pubsub"
 	"github.com/tonjun/wsclient"
 
 	. "github.com/onsi/ginkgo"
@@ -51,18 +52,18 @@ var _ = Describe("ConfigServer", func() {
 
 		client1 *wsclient.WSClient
 		client2 *wsclient.WSClient
-		//client3 *wsclient.WSClient
+		client3 *wsclient.WSClient
 
 		buffer1 *gbytes.Buffer
 		buffer2 *gbytes.Buffer
-		//buffer3 *gbytes.Buffer
+		buffer3 *gbytes.Buffer
 	)
 
 	BeforeEach(func() {
 		log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 		buffer1 = gbytes.NewBuffer()
 		buffer2 = gbytes.NewBuffer()
-		//buffer3 = gbytes.NewBuffer()
+		buffer3 = gbytes.NewBuffer()
 
 		addr := getListenAddress()
 
@@ -78,7 +79,7 @@ var _ = Describe("ConfigServer", func() {
 		// create client connections and send incomding data to buffer
 		client1 = connectClient(addr, buffer1, "client1")
 		client2 = connectClient(addr, buffer2, "client2")
-		//client3 = connectClient(addr, buffer3, "client3")
+		client3 = connectClient(addr, buffer3, "client3")
 
 	})
 
@@ -165,6 +166,79 @@ var _ = Describe("ConfigServer", func() {
 		Consistently(buffer2).ShouldNot(gbytes.Say(
 			`{"op":"peers_changed","type":"push","id":".","peers":\["127\.0\.0\.1:7171"\,"192\.168\.0\.100:7171"]}`,
 		))
+
+		client3.SendJSON(wsclient.M{
+			"op":   "connect",
+			"type": "request",
+			"id":   "req-client-3",
+			"addr": "192.168.0.101:7171",
+		})
+		Eventually(buffer3).Should(gbytes.Say(
+			`{"op":"connect","type":"response","id":"req-client-3","peers":\["127\.0\.0\.1:7171","192\.168\.0\.100:7171","192\.168\.0\.101:7171"],"config":\{"feature1":\{"enable":false\},"feature2":\{"enable":true\}\}}`,
+		))
+
+		Eventually(buffer1).Should(gbytes.Say(
+			`{"op":"peers_changed","type":"push","id":"3","peers":\["127\.0\.0\.1:7171","192\.168\.0\.100:7171","192\.168\.0\.101:7171"\]}`,
+		))
+		Eventually(buffer2).Should(gbytes.Say(
+			`{"op":"peers_changed","type":"push","id":"3","peers":\["127\.0\.0\.1:7171","192\.168\.0\.100:7171","192\.168\.0\.101:7171"\]}`,
+		))
+
+	})
+
+	It("should remove disconnected clients from the memory store", func(done Done) {
+		client1.SendJSON(wsclient.M{
+			"op":   "connect",
+			"type": "request",
+			"id":   "2",
+			"addr": "127.0.0.1:7171",
+		})
+		Eventually(buffer1).Should(gbytes.Say(
+			`{"op":"connect","type":"response","id":"2","peers":\["127\.0\.0\.1:7171"\],"config":\{"feature1":\{"enable":false\},"feature2":\{"enable":true\}\}}`,
+		))
+
+		store := server.GetStore()
+		i, found, _ := store.Get("127.0.0.1:7171")
+		Expect(found).To(Equal(true))
+		Expect(i).ShouldNot(BeNil())
+		c := i.Value.(pubsub.Conn)
+		connID := c.ID()
+		log.Printf("client1 connection id: %d", connID)
+
+		i, found, _ = store.Get(fmt.Sprintf("%d", connID))
+		Expect(found).To(Equal(true))
+		Expect(i).ShouldNot(BeNil())
+
+		client2.SendJSON(wsclient.M{
+			"op":   "connect",
+			"type": "request",
+			"id":   "2",
+			"addr": "192.168.0.100:7171",
+		})
+		Eventually(buffer2).Should(gbytes.Say(
+			`{"op":"connect","type":"response","id":"2","peers":\["127\.0\.0\.1:7171"\,"192\.168\.0\.100:7171"],"config":\{"feature1":\{"enable":false\},"feature2":\{"enable":true\}\}}`,
+		))
+
+		client1.Close()
+		client1.OnClose(func() {
+			defer GinkgoRecover()
+
+			log.Printf("OnClose")
+
+			time.Sleep(10 * time.Millisecond)
+
+			store := server.GetStore()
+			i, found, _ := store.Get("127.0.0.1:7171")
+			Expect(found).To(Equal(false))
+			Expect(i).Should(BeNil())
+
+			i, found, _ = store.Get(fmt.Sprintf("%d", connID))
+			Expect(found).To(Equal(false))
+			Expect(i).Should(BeNil())
+
+			close(done)
+		})
+
 	})
 
 })
